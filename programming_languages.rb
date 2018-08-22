c861c917f324c1e801fe4a753015aa24784cb998
angelica winkler <caw400@nyu.edu>
2018-08-22 17:24:26 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -4,7 +4,9 @@ def reformat_languages (languages)
   new_hash = {}
 
   languages.each do |style, language_hash|
-    binding.pry
+    language_hash.each do |lang_plus_type|
+      binding.pry
+    end
   end
   new_hash
 end
